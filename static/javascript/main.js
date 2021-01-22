a173a9891157dd0df4ae1cb71c7e600cf137eb4b
Mark Tennyson <46404058+marktennyson@users.noreply.github.com>
2021-01-23 00:06:59 +0530

    added domain engagement features.

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -11,9 +11,11 @@ function fetchAPI(){
 	fetch(finalEndpoint).then(res=>res.json()).then(body=>{
 		const domainAlexaRank = body.alexa_rank;
 		const timeTaken = body.time_taken;
-		// const timeTaken = body.
-		console.log(domainAlexaRank);
+		const domainEngagementinS = body.engagement;
+		const domainEngagementinM = (parseFloat(domainEngagementinS)/60).toFixed(2).toString().split(".")[0];
+		const domainEngagementinSf = (parseFloat(domainEngagementinS)%60)
 		document.getElementById("alexaRankSpan").innerHTML = domainAlexaRank;
+		document.getElementById("domainEngagementSpan").innerHTML = domainEngagementinM+" Minutes "+domainEngagementinSf+" Seconds";
 		document.getElementById("domainNameSpan").innerHTML = domainName;
 		document.getElementById("timeTakenSpan").innerHTML = timeTaken+" seconds";
 	})
@@ -21,6 +23,7 @@ function fetchAPI(){
 
 function modalClose(){
 	document.getElementById("alexaRankSpan").innerHTML = "Fetching...";
+	document.getElementById("domainEngagementSpan").innerHTML = "Fetching...";
 	document.getElementById("domainNameSpan").innerHTML = "Fetching...";
 	document.getElementById("timeTakenSpan").innerHTML = "Fetching...";
 }
